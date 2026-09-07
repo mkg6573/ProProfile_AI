b5f3ae8621eea78e3abfe91e4206fd5f2c7c7fa5
mkg6573 <mohitgupta6573@gmail.com>
2026-09-07 23:44:22 +0530

    update project

--- backend/main.py
+++ backend/main.py
@@ -18,7 +18,10 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173","https://proprofile-ai-backend.onrender.com/"],
+    allow_origins=[
+        "http://localhost:5173",
+        "https://mohitaiassistance.onrender.com",
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
